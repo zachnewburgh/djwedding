e83b7890753bd21ee222758b480cd5f39b56c9bd
zachnewburgh <zach.newburgh@gmail.com>
2016-09-29 19:03:12 -0400

    Configured home route and CRUD routes for FAQ

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,2 +1,32 @@
 angular
-  .module('app', ['ui.router', 'ngResource']) 
+  .module('app', ['ui.router', 'ngResource'])
+  .config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+     .state('home', {
+       url: '/',
+       templateUrl: 'home.html',
+       controller: 'HomeController as ctrl'
+     })
+     .state('home.faqs', {
+       url: 'faqs',
+       templateUrl: 'home/faqs.html',
+       controller: 'FaqsController as ctrl'
+     })
+     .state('home.faqs.new', {
+       url: 'faqs/new',
+       templateUrl: 'home/faqs/new.html',
+       controller: 'NewFaqController as ctrl'
+     })
+
+     .state('home.faqs.faq', {
+       url: 'faqs/:id',
+       templateUrl: 'home/faqs/show.html',
+       controller: 'ViewFaqController as ctrl'
+     })
+    .state('home.faqs.edit', {
+     url: 'faqs/edit/:id',
+     templateUrl: 'home/faqs/edit.html',
+     controller: 'EditFaqController as ctrl'
+     });
+  $urlRouterProvider.otherwise('/');
+});
