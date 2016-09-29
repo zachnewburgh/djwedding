e7411e33e0c93525366661ae1d0d931047b8cee8
zachnewburgh <zach.newburgh@gmail.com>
2016-09-29 19:45:03 -0400

    Added delete function

--- app/assets/javascripts/angular/controllers/FaqController.js
+++ app/assets/javascripts/angular/controllers/FaqController.js
@@ -3,6 +3,12 @@ function FaqsController(Faq, $location, $state) {
   var ctrl = this;
   ctrl.faqs = Faq.query();
 
+  ctrl.deleteFaq = function(faq) {
+    Faq.$delete(function() {
+      $state.reload();
+    });
+  };
+
 };
 
 angular
