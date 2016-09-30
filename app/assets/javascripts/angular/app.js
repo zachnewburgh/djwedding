925a82303d1a12d3fbbd100eb49eb29fb34b3ec6
zachnewburgh <zach.newburgh@gmail.com>
2016-09-30 15:26:25 -0400

    Injecting ui.bootstrap

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('app', ['ui.router', 'ngResource', 'templates'])
+  .module('app', ['ui.router', 'ngResource', 'templates', 'ui.bootstrap'])
   .config(function($stateProvider, $urlRouterProvider) {
    $stateProvider
      .state('home', {
